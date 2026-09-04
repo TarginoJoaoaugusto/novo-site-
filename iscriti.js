011ac647bfd7ef7de147bca62c933feb0239c2f4
TarginoJoaoaugusto <joao.targino@escola.pr.gov.br>
2026-09-04 10:29:59 -0300

    i

---
+++ iscriti.js
@@ -0,0 +1,19 @@
+function botaoClicado(){
+    ler texto=
+    botao.querSelector("span");
+    texto.texContent++;
+}
+
+botoes.forEach(funcion(botao)){
+    ler curtir=false;
+    botao.addEventListener("click"botaoClicado);
+    funcion botaoClicado (){
+        console.log("fuiclicado");
+        ler texto=
+        botao.querSelector("span");
+        if (curtiu === false){
+            texto.texContent++;
+            curtiu=true;
+}
+}
+}
